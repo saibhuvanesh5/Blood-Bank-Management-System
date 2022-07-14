b397167ca07e144035c63df6d693ffc19fb024fb
SaiBhuvaneshNamburu <bhuvaneshnamburu5@gmail.com>
2022-07-14 17:14:09 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>Blood Bank Management System<br>
+  Developed by N.Sai Bhuvanesh,N.Praneeth Babu and G.Balaji
+  <br>
+  ALL RIGHTS RESERVED.
+  </center>
+  </div>
+
+
+</body>
+
+</html>
